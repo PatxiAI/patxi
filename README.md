c0eded18a415929c070a6d65f6ffa940de6f7d8b
Ibon Azkoitia <ibon@kreatidos.com>
2026-06-27 16:52:10 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Your Personal AI Assistant
+
+[![Typing SVG](https://readme-typing-svg.demolab.com?font=Roboto&pause=1000&color=FF5200&width=435&lines=Your+Laravel%2C+Your+Data;One+System%2C+Multiple+AI+Providers;Personal+Assistant+customized+for+you)](https://git.io/typing-svg)
+
---
+++ src/PatxiServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PatxiAI\Patxi;
+
+use Illuminate\Support\ServiceProvider;
+
+class PatxiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    #[\Override]
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
